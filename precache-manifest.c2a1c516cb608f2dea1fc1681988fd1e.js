eec8987562259353342767b96364f0e7e26d9a45
amalius-99 <amaltapalov@gmail.com>
2019-07-28 22:30:36 +0200

    Updates

---
+++ precache-manifest.c2a1c516cb608f2dea1fc1681988fd1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68489f4bc025bce45134d41037efd9bf",
+    "url": "/react-boostrap-portfolio/index.html"
+  },
+  {
+    "revision": "0e21e536cf0678658d0c",
+    "url": "/react-boostrap-portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "60beac3993aab7f6ddb3",
+    "url": "/react-boostrap-portfolio/static/js/2.f1326f99.chunk.js"
+  },
+  {
+    "revision": "0e21e536cf0678658d0c",
+    "url": "/react-boostrap-portfolio/static/js/main.f3197b2d.chunk.js"
+  },
+  {
+    "revision": "6fd20a5b80451328143f",
+    "url": "/react-boostrap-portfolio/static/js/runtime~main.73466602.js"
+  },
+  {
+    "revision": "253edb17c55b5900feb688c066b1d48f",
+    "url": "/react-boostrap-portfolio/static/media/profile.253edb17.jpg"
+  }
+]);
