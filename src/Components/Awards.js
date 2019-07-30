9edda06611d3cccb16b86cfb7542fdafbbd2eee7
amaltapalov <amal.t@tabella.cz>
2019-07-30 10:22:14 +0200

    Add small adjustment

--- src/Components/Awards.js
+++ src/Components/Awards.js
@@ -9,7 +9,7 @@ const Awards = props => (
 	>
 		<div class="w-100">
 			<h2 class="mb-5">Awards &amp; Certifications</h2>
-			<ul class="fa-ul mb-0">
+			<ul class="fa-ul mb-0 ml-0">
 				{props.awards.map((award, i) => (
 					<li key={i}>
 						<FontAwesomeIcon icon={faAward} color="#ffc107" />
